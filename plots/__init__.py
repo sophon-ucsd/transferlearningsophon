e780bc4c44431e8976c25625d5dcfcade1c6ba16
raunavm <raunav.mendiratta@icloud.com>
2026-04-27 20:00:13 -0700

    Figures 1-3

--- plots/__init__.py
+++ plots/__init__.py
@@ -1,12 +1,12 @@
 """Shared poster-figure styling."""
 from plots.style import (
-    apply_style, save_fig,
+    set_publishable_style, apply_style, save_fig,
     OKABE_ITO, PALETTE, CLASS_COLORS, STRATEGY_COLORS, PRE_VS_FT,
-    FIG_SIZE,
+    FIG_SIZE, single_col, double_col,
 )
 
 __all__ = [
-    "apply_style", "save_fig",
+    "set_publishable_style", "apply_style", "save_fig",
     "OKABE_ITO", "PALETTE", "CLASS_COLORS", "STRATEGY_COLORS", "PRE_VS_FT",
-    "FIG_SIZE",
+    "FIG_SIZE", "single_col", "double_col",
 ]
